4f9e67c3a1a33be97fe5f87e61122ee8b7210c11
lzyCUMT <3555245738@qq.com>
2025-09-03 14:50:10 +0800

    refactor(02-Fundamentals-Part-2): 将数组操作示例替换为对象操作示例
    - 删除了之前的 friends 数组操作代码
    - 添加了新的 jonas 对象，并展示了对象属性的访问和修改方法
    - 增加了用户输入提示，以交互方式获取对象属性信息
    - 添加了对不存在属性的错误处理
    - 展示了如何使用模板字符串输出对象信息

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,16 +1,24 @@
-const friends = ['MIchael', 'Steven', 'Peter'];
-const newLength = friends.push('Jay');
-console.log(friends);
-console.log(newLength);
-friends.unshift('John');
-console.log(friends);
-friends.pop();
-const popped = friends.pop();
-console.log(popped);
-console.log(friends);
-friends.shift();
-console.log(friends);
-console.log(friends.indexOf('Steven'));
-console.log(friends.indexOf('Bob'));
-console.log(friends.includes('Steven'));
-console.log(friends.includes('Bob'));
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
+console.log(jonas);
+console.log(jonas.lastname);
+console.log(jonas['lastname']);
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+const interstedIn = prompt('what do you what to know about jonas? choose between firstname, lastname, age, job and friends');
+
+if (jonas[interstedIn]) {
+    console.log(jonas[interstedIn]);
+} else {
+    console.log('Wrong request! Choose between irstname, lastname, age, job and friends');
+}
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschemedtman';
+console.log(jonas);
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
