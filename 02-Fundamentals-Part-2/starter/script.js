646a10ed77ec79bed29901752f95f933227cd860
lzyCUMT <3555245738@qq.com>
2025-09-03 13:59:11 +0800

    feat: 添加多个新章节和功能
    - 新增 Fundamentals Part 2 章节，包含 HTML、CSS 和 JavaScript 基础
    - 新增 Developer Skills 章节，涵盖调试和开发技巧
    - 新增 HTML & CSS 章节，深入讲解 HTML 和 CSS 基础
    - 新增 Guess My Number 游戏项目
    - 添加多个新脚本和样式文件
    - 更新项目结构，优化目录组织

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+const friends = ['MIchael', 'Steven', 'Peter'];
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+friends.unshift('John');
+console.log(friends);
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+friends.shift();
+console.log(friends);
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
