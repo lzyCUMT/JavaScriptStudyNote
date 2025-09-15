e3f7e5ea0287e4409b1200f5b0920edf89d1f67e
lzyCUMT <3555245738@qq.com>
2025-09-15 22:57:06 +0800

    feat(Asynchronous): 获取国家数据并渲染邻国信息
    - 新增 renderCountry 函数用于渲染国家信息
    - 新增 getCountryAndNeighbour 函数获取国家及其邻国数据
    - 在获取国家数据后调用 renderCountry 渲染结果
    - 添加邻国数据获取和渲染逻辑
    - 在脚本末尾添加 setTimeout 示例

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -11,7 +11,21 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
@@ -20,19 +34,24 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-      </div>
-    </article>
-    `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
+
+    const neighbour = data.borders?.[0];
+
+    const request2 = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
+getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => console.log('1 second passed'), 1000);
