8f1df173d17667106706122e269c5bf1eac8ffce
lzyCUMT <3555245738@qq.com>
2025-09-16 15:37:04 +0800

    feat(script): 添加 Promise 相关示例代码
    - 注释掉原有的测试代码
    - 添加 lotteryPromise 示例，模拟彩票抽奖过程
    - 实现 wait 函数，用于创建指定时间后的 Promise
    - 使用 then 和 catch 方法处理 Promise 的 resolve 和 reject

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -123,13 +123,48 @@ const countriesContainer = document.querySelector('.countries');
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 
-console.log('Test start');
-setTimeout(() => console.log('Resolved promise 1'), 0);
-Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test start');
+// setTimeout(() => console.log('Resolved promise 1'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
 
-Promise.resolve('Resolved promise 2').then(res => {
-  for (let i = 0; i < 1000000000; i++) {}
-  console.log(res);
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 1000000000; i++) {}
+//   console.log(res);
+// });
+
+// console.log('Test end');
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
 });
 
-console.log('Test end');
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
