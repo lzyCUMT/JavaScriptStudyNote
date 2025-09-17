af8c3bf609970e5b2e2b6287818cf94a7eb4826b
lzyCUMT <3555245738@qq.com>
2025-09-17 20:03:27 +0800

    refactor(shoppingCart): 重构购物车模块
    - 注释掉原有的购物车实现代码
    - 添加新的 ShopingCart2 对象，实现更丰富的购物车功能
    - 新增 orderStock 方法用于从供应商下单
    - 修复了一些拼写错误

--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,23 +1,52 @@
-console.log('Exporing module');
+// console.log('Exporing module');
 
-// console.log('Start fetching user...');
-// await fetching('https://jsonplaceholder.typicode.com/users');
-// console.log('User fetched');
+// // console.log('Start fetching user...');
+// // await fetching('https://jsonplaceholder.typicode.com/users');
+// // console.log('User fetched');
 
-const shippingCost = 10;
-const cart = [];
+// const shippingCost = 10;
+// const cart = [];
 
-export const addToCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
-};
+// export const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
 
-const totalPrice = 237;
-const totalQuantity = 23;
+// const totalPrice = 237;
+// const totalQuantity = 23;
 
-export { totalPrice, totalQuantity as tq };
+// export { totalPrice, totalQuantity as tq };
 
-export default function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
-}
+// export default function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// }
+
+const ShopingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShopingCart2.shippingCost);
