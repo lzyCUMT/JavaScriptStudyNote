44a5601ee8b94b6b0c0da077a3931159507af806
lzyCUMT <3555245738@qq.com>
2025-09-17 19:11:44 +0800

    feat(starter): 添加 ES 模块支持并创建购物车模块
    - 在 index.html 中将 script 标签改为使用 type="module"
    - 新增 script.js 和 shoppingCart.js 文件，实现模块化代码结构
    - 在 shoppingCart.js 中导出 addToCart、totalPrice 和 totalQuantity 函数/变量
    - 在 script.js 中导入并使用 shoppingCart.js 的功能

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,14 @@
+import {
+  addToCart,
+  totalPrice as price,
+  totalQuantity,
+} from './shoppingCart.js';
+('./shoppingCart.js');
+
+import * as ShoppingCart from './shoppingCart.js';
+
+console.log('Importing module');
+
+addToCart('bread', 5);
+console.log(totalPrice, totalQuantity);
+console.log(price);
